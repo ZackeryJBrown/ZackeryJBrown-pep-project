198b0fb08d0ee1d2a058e16c36779b23ea1793d3
ZackeryJBrown <zackeryjbrown@gmail.com>
2023-02-02 17:42:34 +0000

    Account services additions

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -1,5 +1,39 @@
 package DAO;
 
+import Model.Account;
+import Service.AccountService;
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountDAO {
     
-}
+    /**
+    *@return returns all accounts
+    */
+
+    public List<Account> getAllAccounts(){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Account> accounts = new ArrayList<>();
+
+        try{
+            String sql = ";";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while(rs.next()){
+                Account account = new Account(rs.getInt(/*account ID from account table in "" */), rs.getString(/*account name and other items */));
+                accounts.add(account);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        };
+        
+        return accounts;
+        };
+
+};
+
+
--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -5,5 +5,45 @@ import DAO.AccountDAO;
 import java.util.List;
 
 public class AccountService {
+    private AccountDAO accountDAO;
+
+
+
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    };
+
+    //Retrieves all accounts from database
+    public List<Account> getAllAccounts(){
+
+        return accountDAO.getAllAccounts();
+    };
     
+    
+    /**adds an account to the database
+    *
+    *
+    *@param account, an account object
+    *@return returns the added account for confirmation
+    */
+    public Account addAccount(Account account){
+
+        //TODO add logic to check if input was valid, as per use of a service class
+
+        /*
+         * - The registration will be successful if and only if the username is not blank,
+         *  the password is at least 4 characters long, and an Account with that username 
+         * does not already exist. If all these conditions are met, 
+         * the response body should contain a JSON of the Account, including its account_id.
+         *  The response status should be 200 OK, which is the default. 
+         * The new account should be persisted to the database.
+         * If the registration is not successful, the response status should be 400. (Client error)
+         */
+
+
+
+        return accountDAO.insertAccount(account);
+    };
+
+
 }
